d8f394388d241810a0783330a21ab3f8094f704a
haramishra <haraprasad45@gmail.com>
2023-07-25 19:45:13 +0530

    _app configured

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '/src/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "/src/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@/components/theme-provider";
+import { SiteHeader } from "@/components/site-header";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <div className="relative flex min-h-screen flex-col">
+        <SiteHeader />
+        <div className="flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
+      <TailwindIndicator />
+    </ThemeProvider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
+import { Html, Head, Main, NextScript } from "next/document";
+import { cn } from "@/lib/utils";
+import { fontSans } from "@/lib/fonts";
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
